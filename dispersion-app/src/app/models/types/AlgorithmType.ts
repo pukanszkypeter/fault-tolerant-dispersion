41c558005f3c1b4142997e21fbf6cda237e547be
Balázs Bence <balazsbence.sas@gmail.com>
2022-08-29 13:32:55 +0200

    35-dfs-dispersion-frontend
    Frontend integration basics

--- dispersion-app/src/app/models/types/AlgorithmType.ts
+++ dispersion-app/src/app/models/types/AlgorithmType.ts
@@ -6,5 +6,6 @@ export const algorithmTypes: AlgorithmType[] = [
   { value: 'random'},
   { value: 'random_with_leader'},
   { value: 'rotor_router'},
-  { value: 'rotor_router_with_leader'}
+  { value: 'rotor_router_with_leader'},
+  { value: 'dfs_with_global'}
 ];
