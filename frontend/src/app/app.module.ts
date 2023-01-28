e63cab888f9bf8dbf56bcbe76e47d4eff02eb987
Pukánszky Péter <pknszky@protonmail.com>
2023-01-28 11:45:58 +0100

    New frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { MainComponent } from './components/layout/main/main.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SimulatorComponent } from './components/pages/simulator/simulator.component';
+import { TesterComponent } from './components/pages/tester/tester.component';
+import { ChangelogComponent } from './components/pages/changelog/changelog.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { MapSimulatorComponent } from './components/pages/map-simulator/map-simulator.component';
+import { ResultsComponent } from './components/pages/results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent,
+    SimulatorComponent,
+    TesterComponent,
+    ChangelogComponent,
+    PageNotFoundComponent,
+    MapSimulatorComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/pages/map-simulator/map-simulator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-simulator',
+  templateUrl: './map-simulator.component.html',
+  styleUrls: ['./map-simulator.component.scss']
+})
+export class MapSimulatorComponent {
+
+}
