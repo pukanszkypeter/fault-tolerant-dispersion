9e0e9fab2db35e7345a5250893f7e2f60729d6d4
Pukánszky Péter <pknszky@protonmail.com>
2023-02-02 19:58:25 +0100

    #21 Added dependabot.yml

---
+++ backend/src/main/java/hu/elte/ik/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.ik.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
